993a450c06eff5faeb0c1ef4a137e6ffa4976ec3
Gajendra Mahato <62411697+NoobGajen@users.noreply.github.com>
2024-07-27 19:12:51 +0545

    Update script.js
    my role as a pentester removed and Computer Science Student added for college convinent purpose

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed('.typing', {
-    strings: ['Pentester', 'Ethical Hacker', 'Freelancer'],
+    strings: ['Computer Science Student', 'Computer Science Student'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed('.typing-2', {
-    strings: ['Pentester', 'Ethical Hacker', 'Freelancer'],
+    strings: ['Computer Science Student', 'Computer Science Student'],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
